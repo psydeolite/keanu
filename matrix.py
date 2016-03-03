ea0bf27009cda37b51cf67cc2ce98287803b181e
masha <maryged@gmail.com>
2016-03-02 22:44:31 -0500

    matrix multi

--- matrix.py
+++ matrix.py
@@ -71,7 +71,7 @@ def print_matrix( matrix ):
         print row
     #pass
 n=make_scale('a','b','c');
-print_matrix(n);
+#print_matrix(n);
 
 
 def ident( matrix ):
@@ -92,5 +92,54 @@ def scalar_mult( matrix, x ):
 
 #m1 * m2 -> m2
 def matrix_mult( m1, m2 ):
-    pass
+    #print w
+    #l=len(m2)
+    #print l
+    print 'm1 rows: '+str(len(m1))
+    print 'm2 cols: '+str(len(m2[0]))
+    mat=new_matrix(len(m2[0]),len(m1))
+    #print_matrix(m1)
+    ##print ''
+    #print_matrix(m2)
+    print ''
+    #print_matrix(mat)
+    #i=m1 row index
+    for i in range(len(m1)):
+        #j=m2 col index
+        for j in range(len(m2[0])):
+            #k=m2 row index
+            for k in range(len(m2)):
+                print 'i='+str(i)
+                print 'j='+str(j)
+                print 'k='+str(k)
+                mat[i][j]+=m1[i][k]*m2[k][j]
+                print str(m1[i][j])+' * '+str(m2[k][j])+' => '+str(mat[i][j])
+    return mat
 
+#a=new_matrix(4,3)
+#b=new_matrix(2,4)
+
+a=new_matrix(3,2)
+b=new_matrix(2,3)
+
+a[0][0]=1
+a[0][1]=2
+a[0][2]=3
+a[1][0]=4
+a[1][1]=5
+a[1][2]=6
+
+b[0][0]=7
+b[0][1]=8
+b[1][0]=9
+b[1][1]=10
+b[2][0]=11
+b[2][1]=12
+
+print 'matrix 1:'
+print_matrix(a)
+print 'matrix 2:'
+print_matrix(b)
+
+#matrix_mult(a,b)
+print_matrix(matrix_mult(a,b))
